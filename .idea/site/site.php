977ff7dc6d1073613fde854b43da79a740a1bfe6
samantha <sgrooms.webdev@gmail.com>
2022-03-18 13:30:24 -0600

    2:28 3-18-2022

--- .idea/site/site.php
+++ .idea/site/site.php
@@ -7,50 +7,51 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php
-        $phrase = "To be or not to be";
-        $integer = 30;
-        $decimal = 3.53;
-        $booleanVariable = true; //true or false
-        null; //no value
-
-//        echo "hello";
-
-        $characterName = "Lily";
-        $shoes = "little feets";
-        $characterAge = "5";
-        $favoriteThing = "pets";
-        $obsession = "buns";
-
-        echo "My name is $characterName.<br>";
-        echo "I have $shoes.<br>";
-        echo "I am $characterAge years old.<br>";
-        echo "I love $favoriteThing and Dad.<br>";
-        echo "I am obsessed with $obsession.<br>";
 
 
-        //Strings
-        echo "These are all strings in plain text <br>";
-        //Store string inside a variable
-        $exampleofString = "These are all strings in plain text <br>";
-        echo $exampleofString;
-        //String with lower case text
-        echo strtolower($exampleofString);
-        //String with upper case text
-        echo strtoupper($exampleofString);
-        //Shows what character is in this position
-        //echo $exampleofString[7];
-        //Modify individual characters
-        $exampleofString[0] = "S";
-        //String Replace
-        $stringReplace = "This is how you replace a word in a string <br>";
-        echo str_replace("This", "Here", $stringReplace);
 
-        //Sub string
-        echo substr($exampleofString, 14);
+<!--//    $friends = array("Ais", "Betsy", "Liz", "Levi");-->
+<!--//    $friends[4] = "Angie";-->
+<!--//    echo count($friends);-->
+<!--    <form action="site.php" method="post">-->
+<!---->
+<!--        Apples: <input type="checkbox" name="fruits[]" value="apples"><br>-->
+<!--        Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>-->
+<!--        Pears: <input type="checkbox" name="fruits[]" value="pears"><br>-->
+<!--        Blueberries: <input type="checkbox" name="fruits[]" value="blueberries"><br>-->
+<!--        <input type="text" name="student">-->
+
+<!--        <input type="submit">-->
+<!--    </form>-->
 
 
-        ?>
+<!--////       $grades = array("Jim" => "A+", "Pam" => "B-","Oscar" => "C+");-->
+<!--////       echo $grades[$_POST["student"]];-->
+<!--////        function sayHi($name) {-->
+<!--////            echo "Hello $name";-->
+<!--////        }-->
+<!--////-->
+<!--////        echo sayHi("Sam");-->
+<!--//        function cube($num) {-->
+<!--//            return $num * $num * $num;-->
+<!--//        }-->
+<!--//        $cubeResult = cube(4);-->
+<!--//        echo $cubeResult;-->
+    <?php
+    //If Statements
+    $isMale = true;
+    $isTall = true;
+    if ($isMale && $isTall) {
+        echo "You are a TALL male";
+    } else {
+        echo "You are a woman";
+    }
+
+    ?>
+
+
+
+
 
     </body>
 </html>
